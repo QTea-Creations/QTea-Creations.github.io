03bc2dd624385ae22d861139b132fbf72c73ebfe
QTea-Creations <mrsvictoriajoi@gmail.com>
2026-06-10 20:20:24 -0400

    Add mission completion tracking functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+let completedMissions = 0;
+const totalMissions = 3;
+
+function completeMission(missionNumber) {
+  completedMissions++;
+
+  if (completedMissions > totalMissions) {
+    completedMissions = totalMissions;
+  }
+
+  const percent = (completedMissions / totalMissions) * 100;
+
+  document.getElementById("progressText").textContent =
+    completedMissions + " missions completed";
+
+  document.getElementById("progressFill").style.width = percent + "%";
+
+  alert("Mission " + missionNumber + " complete! Badge unlocked!");
+}
