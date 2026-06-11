1dd5acd0bdd18f1c1b47c4f128c1c569afb0860d
QTea-Creations <mrsvictoriajoi@gmail.com>
2026-06-11 11:22:33 -0400

    Remove progress fill width update
    Removed progress fill width update from the UI.

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ function completeMission(missionNumber) {
   document.getElementById("progressText").textContent =
     completedMissions + " of " + totalMissions + " missions complete";
 
-  document.getElementById("progressFill").style.width = percent + "%";
-
   document.getElementById("circleProgress").textContent = percent + "%";
 
   alert("Mission complete! Badge unlocked!");
